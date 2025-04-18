7dfa31cf5f2d223a2ee2d96a9e41fb0960689490
allavinner <joel.w.ottosson@gmail.com>
2025-04-18 22:51:07 +0200

    API: Allow for CORS during development

--- api/main.py
+++ api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "hello World"}
+
+
+@app.get("/api/basic")
+async def basic():
+    return {"message": "This is basic"}
