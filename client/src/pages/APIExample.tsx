30a3694ca1840e9adfdd972162ac72ad2855e4df
allavinner <joel.w.ottosson@gmail.com>
2025-04-18 22:51:42 +0200

    Client: Add example querying from local server.

--- client/src/pages/APIExample.tsx
+++ client/src/pages/APIExample.tsx
@@ -15,10 +15,16 @@ const useStyles = makeStyles({
   }
 })
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 interface BulletItem {
   id: string,
   title: string,
+}
+
+interface Basic {
+  message: string
 }
 
 
@@ -29,16 +35,27 @@ const retrievePosts = async (): Promise<BulletItem[]> => {
   return response.data
 }
 
+const retrieveBasicAPI = async (): Promise<Basic> => {
+  const response = await axios.get(
+    `${API_BASE_URL}/basic`,
+  )
+  return response.data
+}
+
 
 const APIExample: React.FC<PageProps> = (_props) => {
   const styles = useStyles()
   const { data: posts, error, isLoading } = useQuery<BulletItem[], Error>("postsData", retrievePosts);
+  const { data: basic } = useQuery<Basic, Error>("basicAPI", retrieveBasicAPI);
   if (isLoading) return <div> NOT LOADED </div>
   if (error) return <div> GOT ERROR: {error.message} </div>
   if (!posts) return <div> A</div>
 
   return (
     <div className={styles.plotRoot}>
+      <div>
+        {basic?.message}
+      </div>
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
